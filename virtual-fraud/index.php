0134f50836a3910a4c45759acbce0dd62a45966d
Dillon Ellis <dellis@fcul.com>
2022-08-15 16:07:33 -0400

    original virtual fraud index page

---
+++ virtual-fraud/index.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<!--<![endif]-->
+<html lang="en">
+<head>
+<title>LSCU | Virtual Fraud Summit</title>
+<meta charset="utf-8">
+<!-- Meta -->
+<meta name="keywords" content="" />
+<meta name="author" content="">
+<meta name="robots" content="" />
+<meta name="description" content="" />
+
+<!-- this styles only adds some repairs on idevices  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="../../images/favicon.ico">
+
+<!-- Google fonts - witch you want to use - (rest you can just remove) -->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- stylesheets -->
+<link rel="stylesheet" media="screen" href="../../js/bootstrap/bootstrap.min.css" type="text/css" />
+<link rel="stylesheet" href="../../js/megamenu/stylesheets/screen.css" type="text/css" />
+<link rel="stylesheet" href="../../css/default.css" type="text/css" />
+<link rel="stylesheet" href="../../css/layouts.css" type="text/css" />
+<link rel="stylesheet" href="../../css/shortcodes.css" type="text/css" />
+<link rel="stylesheet" href="../../css/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" media="screen" href="../../css/responsive-leyouts.css" type="text/css" />
+<link rel="stylesheet" type="text/css" href="../../css/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
+<link rel="stylesheet" href="../../js/tabs/assets/css/responsive-tabs.css" />
+<link rel="stylesheet" type="text/css" href="../../js/tabs/assets/css/responsive-tabs.css">
+<link rel="stylesheet" href="../../css/et-line-font/et-line-font.css">
+<link rel="stylesheet" href="../../remodal/dist/remodal.css">
+<link rel="stylesheet" href="../../remodal/dist/remodal-default-theme.css">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-12265204-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-12265204-1');
+</script>
+
+</head>
+
+<body>
+<div class="site_wrapper">
+  <div class="topbar light topbar-padding">
+    <?php include '../../inc/topbar.php';?>
+  </div>
+  <div class="clearfix"></div>
+  
+  <div id="header" class="header-section style1 pin-style">
+    <?php include '../../inc/nav.php';?>
+  </div>
+  <!--end menu-->
+  <div class="clearfix"></div>
+  
+  <section>
+    <div class="header-inner two">
+      <div class="inner text-center">
+        <h4 class="titleNEW uppercase">Education</h4>
+		  <h5 class="smtitle uppercase">Virtual Fraud Summit</h5>
+      </div>
+      <img src="../../images/headerBG.png" alt="" class="img-responsive"/> </div>
+  </section>
+  <!-- end header inner -->
+  <div class="clearfix"></div>
+  
+ <section class="sec-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-12 col-xs-12 bmargin">
+          <div class="col-md-12 col-sm-12 col-xs-12 nopadding bmargin">
+            <ul class="category-links">
+				<li><a href="../../about/opportunities.php">Advertising/Sponsorship Opportunities</a></li>
+              <li><a href="../presenters.php">Call for Presenters</a></li>
+              <li><a href="../conferences.php">Conferences & Signature Events</a></li>
+			  <li><a href="../training.php">Schools, Training & Workshops</a></li>
+				<li><a href="../on-demand.php">On Demand</a></li>
+              <li><a href="../education.php">Continuing Professional Education</a></li>
+			  <li><a href="http://lscucouncils.com/" target="_blank">LSCU Councils</a></li>
+			  <li><a href="../../foundation/scholarships.php">Professional Development Grants</a></li>			  
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+			<br>
+		  <div class="col-md-12 col-sm-12 col-xs-12 nopadding bmargin">
+            <div class="feature-box7 text-center active bmargin"> <span class="icon-chat"></span><br/>
+            <br/>
+            <h4 class="uppercase">How Can We Help?</h4>
+            <p>Curious about an educational opportunity? Question about an event? Inquire today.</p>
+            <br/>
+            <a class="btn btn-border light btn-round" href="mailto:education@lscu.coop">Contact Us Now</a></div>
+			  
+          </div>
+          <div class="clearfix"></div>
+          <!--end sidebar box-->
+          
+        </div>
+        <!--end left column-->
+        
+        <div class="col-md-8 col-sm-12 col-xs-12 bmargin">
+			<p align="center"><img src="imgs/header.png" alt="BSA Workshop" class="img-responsive"></p>
+			
+			<p align="center"><strong style="font-size: 20px;">October 5, 2022<br>
+1:00 p.m. - 4:30 p.m.<br>
+$199 per Attendee</strong></p>
+			
+			<p align="center"><a class="sh-btn sh-btn-border blue sh-btn-small sh-btn-round" style="margin-top: 15px;" href="https://na.eventscloud.com/683617" target="_blank">REGISTER NOW</a></p>
+			
+		  <p align="center"><strong>In Partnership With:</strong><br>
+<img src="imgs/cornerstone.png" alt="Nebraska CU League" width="243" height="59"></p>
+			
+		  <h3>Agenda</h3>
+			
+			<table class="table table-striped">
+				  
+				  <thead>
+					  <tr><th width="24%">Time</th>
+					  <th width="76%">Event</th></tr>
+					  </thead>
+					  
+					  <tbody>
+					  <tr>
+					    <td>1:00 p.m. - 1:45 p.m.</td>
+						  <td><p><strong>Welcome</strong></p>
+					      <p><strong>Fraud Trends Roundtable Discussion</strong><br>
+					        Roundtable Facilitator - Sharon Murphy, CIA, CFE, CUCE, CUERME, BSACS, Compliance Consultant, LSCU &amp; Affiliates</p>
+					      <p>Let’s talk fraud. Take part during a lively and interactive discussion on credit union fraud trends and how to combat them. Bring your recent examples of fraud issues and ideas of how to spot and combat these items. Let’s share information and resources to create opportunities to mitigate losses. Let’s dig into various types of fraud we are seeing and network for solutions.</p></td></tr>
+					  <tr>
+					    <td>1:45 p.m. - 2:15 p.m.</td>
+					    <td><p><strong>Internal Fraud: How to Spot It and Stop It</strong><br>
+					      Nicole Irwin, MAcc, Director- Business Development & Client Relations Audit Manager-Alabama, Florida & Georgia, CU AUDIT & COMPLIANCE GROUP (CUACG)</p>
+				        <p>Internal fraud is something that no credit union wants to find. However, it happens much more than you might think. Millions of dollars have been stolen from credit unions in the past- not only by external fraudsters, but also their own staff. Join Nicole Irwin as she discusses internal fraud trends that have happened in credit unions, how credit unions could have spotted the fraud in the very beginning, and how credit unions can stop this fraud from happening to them. </p></td>
+					    </tr>
+					  <tr>
+					    <td>2:15 p.m. - 3:00 p.m.</td>
+					    <td><p><strong>CU Panel Discussion on Existing and Emerging Fraud Trends</strong></p>
+				        <p>Our Panel will discuss new hot button issues happening with fraud, and the most common trends they are seeing today!</p>
+				        <p>Ashley Mobley - CUCE, BSACS, AAP - AVP of Compliance - Listerhill Credit Union<br>
+				          Lynsey Burchett - Director of Fraud Protection - Community First CU
+				        </p></td>
+					    </tr>
+					  <tr>
+					    <td>3:00 p.m. - 3:15 p.m.</td>
+					    <td><strong>Break</strong></td>
+					    </tr>
+					  <tr>
+					    <td>3:15 p.m. - 4:00 p.m.</td>
+					    <td><p><strong>Emerging Fraud Trends Impacting Credit Unions (Zelle P2P, Wire Fraud, Booster Payment Fraud</strong><br>
+					      Holly Spiczenski, AIC, NCRM Consultant, CUNA Mutual Group</p>
+					      <p>Fraudsters continue to plot ways to con or scam your credit unions, your employees and your members. Learn about social engineering tactics and identify steps you can take to minimize the risks.</p></td>
+					    </tr>
+					  <tr>
+					    <td>4:00 p.m. - 4:30 p.m.</td>
+					    <td><p><strong>How to Spot Synthetic Identities Before Fraud Occurs</strong><br>
+					      Sharon Murphy, CIA, CFE, CUCE, CUERME, BSACS, Compliance Consultant, LSCU &amp; Affiliates</p>
+					      <p>Synthetic Identity Fraud is one of the fastest growing financial fraud types in the United States. The Federal Trade Commission defined it in 2021 as <strong>“the use of a combination of personally identifiable information (PII) to fabricate a person or entity in order to commit a dishonest act for personal or financial gain.”</strong> This session will cover how Synthetic Identities are created and some ways that you can help identify them before fraud occurs.</p></td>
+					    </tr>
+					  </tbody>
+				  
+				  </table>
+			
+			
+			<h3>Speakers</h3>
+			<div class="accordion_holder">
+          <div class="accordion_example1">
+			  
+			  <!-- Section 1 -->
+            <div class="accordion_in two">
+              <div class="acc_head">Speakers</div>
+              <div class="acc_content">
+			  	
+				  <table class="table">
+			
+				<tr><td align="center" valign="top"><img src="imgs/nicole-irwin.jpg" alt="Nicole Irwin"/><br>
+				    <strong>Nicole Irwin</strong><br>
+				    <br>
+				    <a class="sh-btn sh-btn-border blue sh-btn-small sh-btn-round" href="#irwin">Learn More</a></td>
+				  <td align="center" valign="top"><img src="imgs/sharon-murphy.jpg" alt="Sharon Murphy" width="94" height="128"/><br><strong>Sharon Murphy</strong><br><br><a class="sh-btn sh-btn-border blue sh-btn-small sh-btn-round" href="#murphy">Learn More</a>
+					</td>
+				  <td align="center" valign="top">
+					<img src="imgs/holly-spiczenski.jpg" alt="Holly Spiczenski" width="101" height="125"/><br><strong>Holly Spiczenski</strong><br><br><a class="sh-btn sh-btn-border blue sh-btn-small sh-btn-round" href="#holly">Learn More</a>
+					</td>
+				  
+				</tr>
+
+			
+			      </table>
+
+			  </div>
+            </div>
+				
+          </div>
+        </div>
+        <!-- Accordion end -->
+
+			
+        </div>
+        <!--end right column--> 
+        
+      </div>
+    </div>
+  </section>
+  <!-- end section -->
+  <div class="clearfix"></div>
+  
+  <section class="section-copyrights sec-moreless-padding">
+	<?php include '../../inc/footer.php';?>
+  </section>
+  <!--end section-->
+  <div class="clearfix"></div>
+  
+  <a href="#" class="scrollup blue"><span style="display:none;">Scroll To Top Button</span></a><!-- end scroll to top of the page--> 
+  
+</div>
+<!--end sitewraper-->
+	
+<div class="remodal" data-remodal-id="irwin" role="dialog" >
+  <button data-remodal-action="close" class="remodal-close" aria-label="Close"></button>
+  <div>
+    <h2>Nicole Irwin</h2>
+    <p>Nicole is the Director of Business Development & Client Relations. She also serves as the Audit Manager of Alabama, Florida, and Georgia.  Nicole Graduated from Austin Peay State University with a Bachelor’s degree in Accounting. In 2020, Nicole graduated from American Military University with her Master’s of Accountancy.  She is currently pursuing her CPA license. Nicole joined the CUACG team in 2017. When not studying auditing, she enjoys golfing and playing with her German Shepherds. Nicole audits credit unions out of Jacksonville, Florida.</p>
+  </div>
+  <br>
+  <button data-remodal-action="cancel" class="remodal-cancel">Close</button>&nbsp;&nbsp;&nbsp;
+</div>
+	
+<div class="remodal" data-remodal-id="murphy" role="dialog" >
+  <button data-remodal-action="close" class="remodal-close" aria-label="Close"></button>
+  <div>
+    <h2>Sharon Murphy</h2>
+    <p>Sharon Murphy has worked in Compliance for over 18 years and in credit unions for over 27 years. Sharon has multiple certifications including Credit Union Compliance Expert; Certified Internal Auditor;  Certified Fraud Examiner, Bank Secrecy Act Compliance Specialist and Credit Union Enterprise Risk Management Expert. She is currently one of LSCU’s Compliance Consultants with client credit unions in Alabama, Florida, and Georgia.</p>
+  </div>
+  <br>
+  <button data-remodal-action="cancel" class="remodal-cancel">Close</button>&nbsp;&nbsp;&nbsp;
+</div>
+	
+<div class="remodal" data-remodal-id="holly" role="dialog" >
+  <button data-remodal-action="close" class="remodal-close" aria-label="Close"></button>
+  <div>
+    <h2>Holly Spiczenski</h2>
+    <p>Holly Spiczenski is a consultant in Risk & Compliance Solutions for CUNA Mutual Group. In this role she assists credit unions in identifying areas of risk in their payments and deposits area of operations and recommends appropriate controls to reduce loss exposure.</p>
+	  
+	  <p>Holly began her career with CUNA Mutual Group specializing in Collateral Protection. She then moved into a role within Business Protection as a claims adjuster. Prior to joining the organization, Holly worked at CUNA Credit Union, which has since evolved into Summit Credit Union.</p>
+  </div>
+  <br>
+  <button data-remodal-action="cancel" class="remodal-cancel">Close</button>&nbsp;&nbsp;&nbsp;
+</div>
+
+
+<!-- ========== Js files ========== --> 
+
+<script type="text/javascript" src="../../js/universal/jquery.js"></script> 
+<script src="../../js/bootstrap/bootstrap.min.js" type="text/javascript"></script> 
+<script src="../../js/megamenu/js/main.js"></script> 
+
+<script src="../../js/scrolltotop/totop.js"></script> 
+<script type="text/javascript" src="../../js/tabs/smk-accordion.js"></script>
+<script type="text/javascript" src="../../js/tabs/custom.js"></script>
+<script src="../../js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+
+<script src="../../remodal/dist/remodal.js"></script>
+
+<!-- Events -->
+<script>
+  $(document).on('opening', '.remodal', function () {
+    console.log('opening');
+  });
+
+  $(document).on('opened', '.remodal', function () {
+    console.log('opened');
+  });
+
+  $(document).on('closing', '.remodal', function (e) {
+    console.log('closing' + (e.reason ? ', reason: ' + e.reason : ''));
+  });
+
+  $(document).on('closed', '.remodal', function (e) {
+    console.log('closed' + (e.reason ? ', reason: ' + e.reason : ''));
+  });
+
+  $(document).on('confirmation', '.remodal', function () {
+    console.log('confirmation');
+  });
+
+  $(document).on('cancellation', '.remodal', function () {
+    console.log('cancellation');
+  });
+</script>
+</body>
+</html>
